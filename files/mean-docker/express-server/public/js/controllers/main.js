4e9aa191816459bebece47742a7755a99d8d88de
LotteWong <LotteWong21@gmail.com>
2019-05-26 16:14:02 +0800

    test view, controller and model

--- files/mean-docker/express-server/public/js/controllers/main.js
+++ files/mean-docker/express-server/public/js/controllers/main.js
@@ -20,12 +20,11 @@ angular.module('todoController', [])
 
 			// validate the formData to make sure that something is there
 			// if form is empty, nothing will happen
-			if ($scope.formData.text != undefined) {
-				//  && $scope.formData.pwd != undefined
+			if ($scope.formData.text != undefined && $scope.formData.pwd != undefined) {
 				$scope.loading = true;
 
 				alert($scope.formData.text);
-				// alert($scope.formData.pwd);
+				alert($scope.formData.pwd);
 
 				// call the create function from our service (returns a promise object)
 				Todos.create($scope.formData)
